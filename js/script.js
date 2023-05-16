45fbdb78e379f7d186fd5a45255944fc1b698955
Italo Gonçalves <116586130+DevIItalo@users.noreply.github.com>
2023-05-15 21:20:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 //Aleatorio
 var imagens = {
     aleatorio: [
-    { caminho: './img/tv.png', nome: 'Assistir qualquer coisa' },
-    { caminho: './img/casar.png', nome: 'Pode casar comigo, mas se eu quiser kakaka' },
-    { caminho: './img/romance.png', nome: 'Ter um Romance' },
-    { caminho: './img/filhos.png', nome: 'Ter filhos' },
-    { caminho: './img/netflix.png', nome: 'Assistir Netflix' },
-    { caminho: './img/hentai.png', nome: 'Assistir hentai/pôrno'},],
+    { caminho: 'img/tv.png', nome: 'Assistir qualquer coisa' },
+    { caminho: 'img/casar.png', nome: 'Pode casar comigo, mas se eu quiser kakaka' },
+    { caminho: 'img/romance.png', nome: 'Ter um Romance' },
+    { caminho: 'img/filhos.png', nome: 'Ter filhos' },
+    { caminho: 'img/netflix.png', nome: 'Assistir Netflix' },
+    { caminho: 'img/hentai.png', nome: 'Assistir hentai/pôrno'},],
    bj: [
-    { caminho: '../gif/Beijo-apaixonado.gif', nome: 'Me dar um beijo apaixonado'},
-    { caminho: '../gif/Beijo-cinema.gif', nome: 'Me dar um beijo conematrográfico'},
-    { caminho: '../gif/Beijo-timido.gif', nome: 'Assistir filme/serie' },
-    { caminho: '../img/beijo.png', nome: 'Assistir filme/serie' }],
+    { caminho: 'gif/Beijo-apaixonado.gif', nome: 'Me dar um beijo apaixonado'},
+    { caminho: 'gif/Beijo-cinema.gif', nome: 'Me dar um beijo conematrográfico'},
+    { caminho: 'gif/Beijo-timido.gif', nome: 'Assistir filme/serie' },
+    { caminho: 'img/beijo.png', nome: 'Assistir filme/serie' }],
     sx: [
-        { caminho: '../img/69-em-ponte.png', nome: '69 em ponte'},
-        { caminho: '../img/Bicicleta.png', nome: 'Bicicleta'},
-        { caminho: '../img/caraacara.png', nome: 'Cara a cara' },
-        { caminho: '../img/cowGirl.png', nome: 'Cow Girl' },
-        { caminho: '../img/david-Copperfield.png', nome: 'David Copperfield' },
-        { caminho: '../img/mariposa.png', nome: 'Mariposa' },
-        { caminho: '../img/o-cachorro.png', nome: 'Cow Girl' },
-        { caminho: '../img/o-estandarte.png', nome: 'O Estandarte'},
-        { caminho: '../img/uma-subita.png', nome: 'Uma Subita'},
-        { caminho: '../img/vaqueira-invertida.png', nome: 'Vaqueira Invertida'},
+        { caminho: 'img/69-em-ponte.png', nome: '69 em ponte'},
+        { caminho: 'img/Bicicleta.png', nome: 'Bicicleta'},
+        { caminho: 'img/caraacara.png', nome: 'Cara a cara' },
+        { caminho: 'img/cowGirl.png', nome: 'Cow Girl' },
+        { caminho: 'img/david-Copperfield.png', nome: 'David Copperfield' },
+        { caminho: 'img/mariposa.png', nome: 'Mariposa' },
+        { caminho: 'img/o-cachorro.png', nome: 'Cow Girl' },
+        { caminho: 'img/o-estandarte.png', nome: 'O Estandarte'},
+        { caminho: 'img/uma-subita.png', nome: 'Uma Subita'},
+        { caminho: 'img/vaqueira-invertida.png', nome: 'Vaqueira Invertida'},
     ]
     };
 
